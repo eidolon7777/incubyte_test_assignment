0a2342d5d2eaff9b2edc740085854e9c466f759a
eidolon7777 <shubhtyagi.work@gmail.com>
2026-01-15 04:27:21 +0530

    Initial setup and empty string test

---
+++ test/string_calculator_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+import 'package:incubyte_test_assesment/string_calculator.dart';
+
+void main() {
+  late StringCalculator calculator;
+
+  setUp(() {
+    calculator = StringCalculator();
+  });
+
+}
